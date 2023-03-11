26c98fb0b6a3ff1b5cd5f785d46f062f64b0a582
sukmawatii <127372014+sukmawatii@users.noreply.github.com>
2023-03-11 04:56:06 -0800

    Create BacaData.java

---
+++ BacaData.java
@@ -0,0 +1,26 @@
+/*NIM   : 13020210025
+Nama    : Sukmawati
+Hari/Tgl: Minggu, 10 Maret 2023
+Waktu   : 20:41 PM*/
+package tugas4;
+import java.util.Scanner;
+
+public class BacaData {
+
+    /**
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        //TODO Auto-generated method stub
+        /* Kamus */
+        int a;
+        Scanner masukan;
+        /* Program */
+        System.out.print("Contoh membaca dan menulis, ketik nilai integer : \n");
+        masukan = new Scanner(System.in);
+        a = masukan.nextInt(); /* coba ketik : masukan.nextInt(); Apa akibatnya?*/
+        System.out.print("Nilai yang dibaca : " +a);
+    }
+
+}
