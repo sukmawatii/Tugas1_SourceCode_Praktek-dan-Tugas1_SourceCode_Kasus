7361d90cd128d04c503d16bf359ed7551f6ef27b
sukmawatii <127372014+sukmawatii@users.noreply.github.com>
2023-03-11 04:55:00 -0800

    Create ASIGNi.java

---
+++ ASIGNi.java
@@ -0,0 +1,38 @@
+/*NIM   : 13020210025
+Nama    : Sukmawati
+Hari/Tgl: Minggu, 10 Maret 2023
+Waktu   : 20:27 PM*/
+package tugas1;
+
+public class ASIGNi {
+
+    public static void main(String[] args) {
+        //TODO Auto-generated method stub
+        /* KAMUS */
+        short ks = 1;
+        int ki = 1;
+        long kl = 10000;
+        char c = 65;    /* inisialisasi karakter dengan integer */
+        char cl ='Z';   /* inisialisasi karakter dengan karakter */
+        double x = 50.2f;
+        float y = 50.2f;
+
+        /* Algoritma */
+
+        /* penulisan karakter sebagai integer */
+        System.out.println("Karakter = " +c);
+        System.out.println("Karakter = " +cl);
+
+        /* penulisan karakter sebagai integer */
+        System.out.println("Karakter = " +c);
+        System.out.println("Karakter = " +cl);
+
+        System.out.println("Bilangan integer (short) = " +ks);
+        System.out.println("\t(int) = " +ki);
+        System.out.println("\t(long) = " +kl);
+        System.out.println("Bilangan Real x = " +x);
+        System.out.println("Bilangan Real y = " +y);
+
+    }
+
+}
