7adb25cfc70f6a063edc42e64699fd5433bf82c8
sukmawatii <127372014+sukmawatii@users.noreply.github.com>
2023-03-11 04:52:49 -0800

    Create Asgdll.java

---
+++ Asgdll.java
@@ -0,0 +1,24 @@
+/*NIM   : 13020200119
+Nama    : Sukmawati
+Hari/Tgl: Jumat, 10 Maret 2023
+Waktu   : 19:47 PM*/
+package tugas1;
+
+public class Asgdll {
+
+    /**
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Kamus */
+        float f=20.0f;
+        double fll;
+
+        /* Algoritma */
+        fll= 10.0f;
+        System.out.println("f : " +f+ "\nfll: " +fll);
+    }
+
+}
