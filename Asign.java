01c14f3606172782387876301b95fa2fb04529d8
sukmawatii <127372014+sukmawatii@users.noreply.github.com>
2023-03-11 04:53:41 -0800

    Create Asign.java

---
+++ Asign.java
@@ -0,0 +1,23 @@
+/*NIM   : 13020210025
+Nama    : Sukmawati
+Hari/Tgl: Minggu, 10 Maret 2023
+Waktu   : 20:17 PM*/
+package tugas1;
+
+public class Asign {
+
+    /**
+     * @param args
+     */
+
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* kamus */
+        int i;
+        /* Program */
+        System.out.print("hello\n"); i=5;
+        System.out.println("Ini nilai i : " +i);
+    }
+
+}
