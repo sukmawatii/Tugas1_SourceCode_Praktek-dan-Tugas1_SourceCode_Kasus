c8c577de282359ef710920029671951e9d49d1a6
sukmawatii <127372014+sukmawatii@users.noreply.github.com>
2023-03-11 05:21:28 -0800

    Create Oper3.java

---
+++ Oper3.java
@@ -0,0 +1,22 @@
+/*NIM   : 13020210025
+Nama    : Sukmawati
+Hari/Tgl: Jumat, 13 Maret 2022
+Waktu   : 21:30 PM*/
+package tugas1;
+
+public class Oper3 {
+
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Algoritma */
+        if (true && true){ System.out.println(true && true); }      /* true = true and true */
+        if (true & true) { System.out.println(true & false); }      /* true & true */
+        if (true) { System.out.println(true); }                     /* true */
+        if (true || true){ System.out.println(true); }              /* true = true or true */
+        if (true|false) { System.out.println(true|false); }         /* true|false */
+
+    }
+}
