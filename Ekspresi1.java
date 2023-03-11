19983c733bb4797f3b3f0cbaeef1c774af3ce575
sukmawatii <127372014+sukmawatii@users.noreply.github.com>
2023-03-11 05:15:30 -0800

    Create Ekspresi1.java

---
+++ Ekspresi1.java
@@ -0,0 +1,39 @@
+/*NIM   : 13020210025
+Nama    : Sukmawati
+Hari/Tgl: Jumat, 10 Maret 2023
+Waktu   : 21:04 PM*/
+package tugas1;
+
+/* pembagian integer casting */
+public class Ekspresi1 {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args){
+        // TODO Auto-generated method stub
+        /* KAMUS */
+        int x = 1; int y = 2; float fx; float fy;
+
+        /* ALGORITMA */
+        System.out.print ("x/y (format integer) = "+ x/y);
+        System.out.print ("\nx/y (format float) = "+ x/y);
+        /* Supaya hasilnya tidak nol */
+        fx=x;
+        fy=y;
+
+        System.out.print ("\nx/y (format integer) = "+ fx/fy);
+        System.out.print ("\nx/y (format float) = "+fx/fy);
+        /* casting */
+
+        System.out.print ("\nfloat(x)/float(y) (format integer) = "+(float)x/(float)y);
+        System.out.print ("\nfloat(x)/float(y) (format float) = "+(float)x/(float)y);
+
+        x = 10; y = 3;
+
+        System.out.print ("\nx/y (format integer) = "+ x/y);
+        System.out.print ("\nx/y (format float)   = "+ x/y);
+
+    }
+
+}
