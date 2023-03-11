0477619cf1ec1860c4a94eb147a423b8cce7de4c
sukmawatii <127372014+sukmawatii@users.noreply.github.com>
2023-03-11 05:17:19 -0800

    Create lncr.java

---
+++ lncr.java
@@ -0,0 +1,25 @@
+/*NIM   : 13020210025
+Nama    : Sukmawati
+Hari/Tgl: Jumat, 10 Maret 2023
+Waktu   : 21:12 PM*/
+package tugas1;
+
+/* Effek dari operator ++ */
+public class Incr {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Kamus */
+        int i, j;
+
+        /* Program */
+        i = 3;
+        j = i++;
+
+        System.out.println("Nilai i : " + (++i) + "\nNilai j : " + j);
+    }
+
+}
