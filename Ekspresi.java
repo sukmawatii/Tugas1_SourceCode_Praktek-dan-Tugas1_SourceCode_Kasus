ca3ee23be7ceb8bd297a913e168b5dffd0da9379
sukmawatii <127372014+sukmawatii@users.noreply.github.com>
2023-03-11 05:14:34 -0800

    Create Ekspresi.java

---
+++ Ekspresi.java
@@ -0,0 +1,24 @@
+/*NIM   : 13020210025
+Nama    : Sukmawati
+Hari/Tgl: Jumat, 10 Maret 2023
+Waktu   : 21:01 PM*/
+package tugas1;
+
+/* Pemakaian operator kondisional */
+public class Ekspresi {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* KAMUS */
+        int x = 1;
+        int y = 2;
+        /* ALGORITMA */
+        System.out.print("x = "+ x + "\n");
+        System.out.print("y = "+ y + "\n");
+        System.out.print("hasil ekspresi = (x<y)?x:y = "+ ((x < y) ? x : y)); 
+        /*Gunakan dalam kurung "(statement dan kondisi)" untuk menyatakan satu kesatuan pernyataan*/
+    }
+}
